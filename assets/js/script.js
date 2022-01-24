742285d0ca7512cb238dbecd13a1ca0da4919923
Neh Joshi <nehjoshi5@gmail.com>
2022-01-24 12:03:40 +0530

    More changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,9 @@ $(window).scroll(function() {
 var theta = $(window).scrollTop() / 600 % (2 * Math.PI);
 $('#logo').css({ transform: 'rotate(' + theta + 'rad)' });
 });
+const ck = document.getElementById("checkbox");
+ck.addEventListener("change", () => {
+    document.getElementsByTagName("nav")[0].classList.toggle("dark");
+    document.getElementById("wave").setAttribute("fill", ck.checked ? "#0b1339" : "#003b4d");
+    document.getElementsByTagName("main")[0].classList.toggle("dark");
+})
