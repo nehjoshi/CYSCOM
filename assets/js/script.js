2b47f56ef1ab2a713d9f6afb666aee53f4b11d16
Neh Joshi <nehjoshi5@gmail.com>
2022-01-24 23:56:05 +0530

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".main", {
+    scrollTrigger: {
+        trigger: ".main",
+        start: 'bottom center',
+        markers: true,
+        toggleActions: "play none none reverse",
+        scrub: 1
+    },
+    opacity: 0.4,
+    ease: Power2.easeInOut
+})
+
+
+
 $(window).scroll(function() {
 var theta = $(window).scrollTop() / 600 % (2 * Math.PI);
 $('#logo').css({ transform: 'rotate(' + theta + 'rad)' });
